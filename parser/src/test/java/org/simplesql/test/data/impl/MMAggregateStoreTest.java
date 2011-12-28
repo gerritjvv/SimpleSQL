fcbd5fd84433fc6566d709beabb22464e4da21cd
gerritjvv <gerritjvv@gmail.com>
2011-12-28 23:20:11 +0100

    mm map

---
+++ parser/src/test/java/org/simplesql/test/data/impl/MMAggregateStoreTest.java
@@ -0,0 +1,21 @@
+package org.simplesql.test.data.impl;
+
+import org.junit.Test;
+import org.simplesql.data.DataEntry;
+
+import vanilla.java.collections.HugeArrayBuilder;
+import vanilla.java.collections.HugeMapBuilder;
+import vanilla.java.collections.api.HugeArrayList;
+
+public class MMAggregateStoreTest {
+
+	@Test
+	public void testStore() throws Throwable {
+		
+		
+		HugeMapBuilder<String, DataEntry> obj = new HugeMapBuilder<String, DataEntry>();
+		
+		
+	}
+	
+}

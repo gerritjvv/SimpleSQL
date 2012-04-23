d156527ca76ebb03b929a8247020e32a54f9be0b
gerritjvv <gerritjvv@gmail.com>
2012-04-23 20:48:02 +0200

    performance test on datasource

--- parser/src/main/java/org/simplesql/data/util/STDINDataSource.java
+++ parser/src/main/java/org/simplesql/data/util/STDINDataSource.java
@@ -1,8 +1,10 @@
 package org.simplesql.data.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.commons.io.IOUtils;
@@ -22,22 +24,68 @@ public class STDINDataSource implements DataSource {
 	final Iterator<String> it;
 
 	final SelectTransform transform;
-	
+	final StreamTokenizer tok;
+
 	public STDINDataSource(SelectTransform transform, String sep) {
 		super();
+
 		this.transform = transform;
 		this.sep = sep;
 		it = IOUtils.lineIterator(new BufferedReader(new InputStreamReader(
 				System.in)));
-		
-//		tok = new StreamTokenizer(new BufferedReader(new InputStreamReader(
-//				System.in)));
-//		
-		
+
+		tok = new StreamTokenizer(new BufferedReader(new InputStreamReader(
+				System.in)));
+
+		tok.whitespaceChars(sep.charAt(0), sep.charAt(0));
+
 	}
 
 	public Iterator<Object[]> iterator() {
-		return new SplitIterator(transform, it, sep);
+
+		return new Iterator<Object[]>() {
+
+			Object[] arr;
+
+			@Override
+			public boolean hasNext() {
+
+				int token;
+				ArrayList<Object> list = new ArrayList<Object>(10);
+
+				try {
+					while ((token = tok.nextToken()) != StreamTokenizer.TT_EOF
+							&& token == StreamTokenizer.TT_EOL) {
+						list.add(tok.sval);
+					}
+
+					if (token == StreamTokenizer.TT_EOF)
+						arr = null;
+					else
+						arr = list.toArray();
+
+				} catch (IOException e) {
+					throw new RuntimeException(e.toString(), e);
+				}
+
+				return (arr != null);
+			}
+
+			@Override
+			public Object[] next() {
+				return arr;
+			}
+
+			@Override
+			public void remove() {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		// return new SplitIterator(transform, it, sep);
+
 	}
 
 	public long getEstimatedSize() {
